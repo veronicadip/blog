998c04d5fd0fe448fe8c15b2afc13d9fe94f422a
veronicadip <98916486+veronicadip@users.noreply.github.com>
2022-04-20 13:21:10 -0300

    google authentication added (#10)
    sign in and sign out options with google

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -2,32 +2,45 @@ import { Component } from "react";
 import { Link } from "react-router-dom";
 
 class Home extends Component {
-    state = {
-        isLoggedIn: false
-    }
+  state = {
+    isLoggedIn: false,
+  };
   componentDidMount() {
     window.gapi.load("client:auth2", () => {
       window.gapi.client
         .init({
           apiKey: "AIzaSyDYXml006Hj3GNvIkiSlOk6FklzKtk054M",
           discoveryDocs: [
-            "https://www.googleapis.com/discovery/v1/apis/translate/v2/rest",
+            "https://blogger.googleapis.com/$discovery/rest?version=v2",
           ],
-          clientId: "my-blog-project-1650306479512",
-          scope: ["https://www.googleapis.com/auth/blogger"],
+          clientId:
+            "524350509394-02lt9mikkjuiea852kj4da9aj3ctibeq.apps.googleusercontent.com",
+          scope: "https://www.googleapis.com/auth/blogger",
         })
         .then(() => {
-          window.gapi.auth2.getAuthInstance().isSignedIn.listen(this.updateSigninStatus);
-          this.updateSigninStatus(window.gapi.auth2.getAuthInstance().isSignedIn.get());
+          window.gapi.auth2
+            .getAuthInstance()
+            .isSignedIn.listen(this.updateSigninStatus);
 
+          this.updateSigninStatus(
+            window.gapi.auth2.getAuthInstance().isSignedIn.get()
+          );
         });
     });
   }
 
   updateSigninStatus = (isLoggedIn) => {
-      this.setState({
-          isLoggedIn: isLoggedIn
-      })
+    this.setState({
+      isLoggedIn: isLoggedIn,
+    });
+  };
+
+  signIn() {
+    window.gapi.auth2.getAuthInstance().signIn();
+  }
+
+  signOut() {
+    window.gapi.auth2.getAuthInstance().signOut();
   }
 
   render() {
@@ -35,13 +48,18 @@ class Home extends Component {
       <div className="home">
         <Link to="/">All posts</Link> |{" "}
         <Link to="/post/new">Add a new post</Link>
-        user is logged in: {this.state.isLoggedIn}
+        {!this.state.isLoggedIn && (
+          <button onClick={this.signIn}>Sign In</button>
+        )}
+        {this.state.isLoggedIn && (
+          <button onClick={this.signOut}>Sign Out</button>
+        )}
       </div>
     );
   }
 }
 
 // blogger API key: AIzaSyDYXml006Hj3GNvIkiSlOk6FklzKtk054M
-// client ID: my-blog-project-1650306479512
+// project ID: my-blog-project-1650306479512
 
 export default Home;
