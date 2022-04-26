f2010e2f9a5c79305692da63d5c6945735ec1348
veronicadip <dipmveronica@gmail.com>
2022-04-26 13:19:07 -0300

    I deleted the index that was wrong

--- src/index.js
+++ src/index.js
@@ -3,13 +3,15 @@ import Home from "./routes/Home";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import NewPost from "./routes/NewPost";
 
+// /blog/:blogId/post/:postId
+
 const rootElement = document.getElementById("root");
 render(
   <BrowserRouter>
     <Routes>
       <Route path="/post/new" element={<NewPost />} />
       <Route path="/" element={<Home />} />
-      <Route path="*" element={<index> <h2>Error 404: page not found.</h2> </index>} />
+      <Route path="*" element={<h2>Error 404: page not found.</h2>} />
     </Routes>
   </BrowserRouter>,
   rootElement
