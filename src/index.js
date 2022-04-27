af922fd1e3dc62cfcb9a0a713d6fdad75826a2bf
veronicadip <98916486+veronicadip@users.noreply.github.com>
2022-04-27 11:58:33 -0300

    error 404 added as a new route (#8)
    * error 404 added as a new route
    
    * I deleted the index that was wrong
    
    * deleted unrelated code
    
    Co-authored-by: Vero <vero@Robertos-Mac-mini-2.local>

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ render(
     <Routes>
       <Route path="/post/new" element={<NewPost />} />
       <Route path="/" element={<Home />} />
+      <Route path="*" element={<h2>Error 404: page not found.</h2>} />
     </Routes>
   </BrowserRouter>,
   document.getElementById("root")
