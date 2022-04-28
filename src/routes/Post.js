1375d4d86569d5702d8b57c4c60115e5f78abd86
veronicadip <dipmveronica@gmail.com>
2022-04-28 13:51:47 -0300

    added content of the post
    added profile, title, post, date and number of comments

--- src/routes/Post.js
+++ src/routes/Post.js
@@ -1,7 +1,5 @@
 import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
 import { useParams } from "react-router-dom";
-
 
 function Post() {
   const params = useParams();
@@ -24,44 +22,66 @@ function Post() {
           scope: "https://www.googleapis.com/auth/blogger",
         })
         .then(() => {
-          window.gapi.client.blogger.posts
-            .list({ blogId: blogId })
-            .then((data) => {
-              setPostData(data.result.items.find((obj) => obj.id === postId));
+          window.gapi.client
+            .request({
+              path: `https://www.googleapis.com/blogger/v3/blogs/${blogId}/posts/${postId}?key=AIzaSyDYXml006Hj3GNvIkiSlOk6FklzKtk054M`,
+            })
+            .then((response) => {
+              console.log(response);
+              setPostData(response);
               setIsLoadingPost(false);
             })
-            .catch(() => setPostError(true), setIsLoadingPost(false));
+            .catch(() => {
+              setPostError(true);
+              setIsLoadingPost(false);
+            });
         });
     });
   }, []);
 
-  const renderPostTest = () => {
-    if (isLoadingPost) {
-      return (
-        <div>
-          <span>Loading...</span>
-        </div>
-      )
-    }
-    if (postError) {
-      return (
-        <div>
-          <span>There was an error loading this post, please try again.</span>
-        </div>
-      )
-    }
+  const renderComments = () => {
+      if (postData.result.replies.totalItems === "0") {
+          return <p>There aren't any comments yet.</p>
+      }
+      return <p>{postData.result.replies.totalItems} comments</p>
+  }
+
+  const renderPublishedDate = () => {
+      const publishedDate = new Date(postData.result.published)
+      return publishedDate.toLocaleString()
+  }
+
+  
+
+  if (isLoadingPost) {
     return (
       <div>
-        <h2>{postData.title}</h2>
-        <div dangerouslySetInnerHTML={{ __html: postData.content }} />
+        <span>Loading...</span>
       </div>
     );
   }
-  return renderPostTest()
-  
+  if (postError) {
+    return (
+      <div>
+        <span>There was an error loading this post, please try again.</span>
+      </div>
+    );
+  }
+  return (
+    <div>
+        <div className="postAuthor">
+      <img src={postData.result.author.image.url} alt="profile picture of the author of this post"/>
+      <span>{postData.result.author.displayName}</span>
+      </div>
+      <h1>{postData.result.title}</h1>
+      <p>Published: {renderPublishedDate()}</p>
+      <div dangerouslySetInnerHTML={{ __html: postData.result.content }} />
+      {renderComments()}
+    </div>
+  );
 }
 
 // window.gapi.client.blogger.posts.list({ blogId: "8309785320197399506" }).then((xx) => console.log(xx))
-/* 
-*/
+/*
+ */
 export default Post;
