d03cad5cb92f3986c999a03445169d136446019d
veronicadip <dipmveronica@gmail.com>
2022-05-03 12:06:27 -0300

    add new route to delete posts

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import Home from "./routes/Home";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import NewPost from "./routes/NewPost";
 import Post from "./routes/Post"
+import DeletePost from "./routes/DeletePost"
 
 render(
   <BrowserRouter>
@@ -11,6 +12,7 @@ render(
       <Route path="/blog/:blogId/post/new" element={<NewPost />} />
       <Route path="/blog/:blogId/post/:postId" element={<Post />} />
       <Route path="*" element={<h2>Error 404: page not found.</h2>} />
+      <Route path="/blog/:blogId/post/:postId/delete" element={<DeletePost />} />
     </Routes>
   </BrowserRouter>,
   document.getElementById("root")
